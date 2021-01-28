4abeebb63e1b54e19686419973ca60d65c1a8eb9
rishi <laxman.siri@gmail.com>
2021-01-28 15:06:46 -0800

    calculator

--- CoderDojo/src/com/RISHI/INTERNET/Calculator.java
+++ CoderDojo/src/com/RISHI/INTERNET/Calculator.java
@@ -1,31 +1,38 @@
 package com.RISHI.INTERNET;
+
 public class Calculator {
-	int answer=0;
+	int answer = 0;
+
 	public Calculator add(int number) {
-		answer+=number;
+		answer += number;
 		return this;
 	}
+
 	public Calculator substract(int number) {
-		answer-=number;
+		answer -= number;
 		return this;
 	}
+
 	public Calculator multiply(int number) {
-		answer*=number;
+		answer *= number;
 		return this;
 	}
+
 	public Calculator divide(int number) {
-		answer/=number;
+		answer /= number;
 		return this;
 	}
-	public int answer(){
+
+	public int answer() {
 		return answer;
 	}
-	public static void main(String[] args){
 
-		Calculator math=new Calculator();
-		int total=math.add(10).substract(5).multiply(3).add(3).answer();
-		System.out.println("Answer: "+total);
-		
+	public static void main(String[] args) {
+
+		Calculator math = new Calculator();
+		int total = math.add(10).substract(5).multiply(3).add(3).answer();
+		System.out.println("Answer: " + total);
+
 	}
 
 }
